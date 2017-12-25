79b13dc9161cf2e802e170ae5476d7e4eec9cc6b
simondlevy <simon.d.levy@gmail.com>
2017-12-25 17:04:01 -0500

    Added comment to address filmo's question

--- examples/SensorFusion/SensorFusion.ino
+++ examples/SensorFusion/SensorFusion.ino
@@ -44,7 +44,11 @@ class Fuser : public TinyEKF {
             F[0][0] = 1;
             F[1][1] = 1;
 
-            // Measurement function
+            // Measurement function simplifies the relationship between state and sensor readings for convenience.
+            // A more realistic measurement function would distinguish between state value and measured value; e.g.:
+            //   hx[0] = pow(this->x[0], 1.03);
+            //   hx[1] = 1.005 * this->x[1];
+            //   hx[2] = .9987 * this->x[1] + .001;
             hx[0] = this->x[0]; // Barometric pressure from previous state
             hx[1] = this->x[1]; // Baro temperature from previous state
             hx[2] = this->x[1]; // LM35 temperature from previous state
