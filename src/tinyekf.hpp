41e604ddcc7f32e5544448f9c8e449a81741232d
simondlevy <simon.d.levy@gmail.com>
2024-05-10 23:48:02 -0400

    =Cleanup

--- src/tinyekf.hpp
+++ src/tinyekf.hpp
@@ -13,6 +13,132 @@
 
 class TinyEkf {
 
+    public:
+
+        void initialize(
+                const uint32_t nowMsec,
+                const uint32_t predictionIntervalMsec,
+                const float min_covariance, 
+                const float max_covariance)
+        {
+            _predictionIntervalMsec = predictionIntervalMsec;
+
+            _isUpdated = false;
+
+            _min_covariance = min_covariance;
+            _max_covariance = max_covariance;
+
+            float diag[EKF_N] = {};
+
+            initialize_covariance_diagonal(diag);
+
+            for (uint8_t i=0; i<EKF_N; ++i) {
+
+                for (uint8_t j=0; j<EKF_N; ++j) {
+
+                    _p[i][j] = i==j ? diag[i] : 0;
+                }
+
+                _x[i] = 0;
+            }
+
+        }
+
+        void predict(const uint32_t nowMsec)
+        {
+            _isUpdated = true;
+
+            float xnew[EKF_N] = {};
+            float F[EKF_N][EKF_N] = {};
+            float Q[EKF_N][EKF_N] = {};
+            get_prediction(nowMsec, _x, xnew, F, Q);
+
+            // $\hat{x}_k = f(\hat{x}_{k-1})$
+            for (uint8_t i=0; i<EKF_N; ++i) {
+                _x[i] = xnew[i];
+            }
+
+            // # $P_k = F_{k-1} P_{k-1} F^T_{k-1} + Q_{k-1}$
+            multiplyCovariance(F);
+            add(_p, Q, _p);
+
+            cleanupCovariance();
+        }
+
+        void update(
+                const float hdat[EKF_N], 
+                const float error, 
+                const float stdMeasNoise)
+        {
+            float h[EKF_N] = {};
+            memcpy(h, hdat, EKF_N*sizeof(float));
+            float ph[EKF_N] = {};
+            multiply(_p, h, ph);
+            const auto r = stdMeasNoise * stdMeasNoise;
+            const auto hphr = r + dot(h, ph); // HPH' + R
+
+            float g[EKF_N] = {};
+            for (uint8_t i=0; i<EKF_N; ++i) {
+                g[i] = ph[i] / hphr;
+            }
+
+            // $\hat{x}_k = \hat{x_k} + G_k(z_k - h(\hat{x}_k))$
+            for (uint8_t i=0; i<EKF_N; ++i) {
+                _x[i] += g[i] * error;
+            }
+
+            float GH[EKF_N][EKF_N] = {};
+            outer(g, h, GH); 
+
+            for (int i=0; i<EKF_N; i++) { 
+                GH[i][i] -= 1;
+            }
+
+            // $P_k = (I - G_k H_k) P_k$
+            multiplyCovariance(GH);
+
+            // Add the measurement variance 
+            for (int i=0; i<EKF_N; i++) {
+                for (int j=0; j<EKF_N; j++) {
+                    _p[i][j] += j < i ? 0 : r * g[i] * g[j];
+                }
+            }
+
+            cleanupCovariance();
+
+            _isUpdated = true;
+        }
+
+        void finalize(void)
+        {
+            if (_isUpdated) {
+
+                float A[EKF_N][EKF_N] = {};
+
+                // Move attitude error into attitude if any of the angle errors are
+                // large enough
+                if (did_finalize(_x, A)) {
+
+                    multiplyCovariance(A);
+
+                    cleanupCovariance();
+                }
+
+                _isUpdated = false;
+            }
+        }
+
+        float * getState(void)
+        {
+            static float x[EKF_N];
+
+            for (uint8_t i=0; i<EKF_N; ++i) {
+                x[i] = _x[i];
+            }
+
+            return x;
+        }
+
     private:
 
         static void transpose(
@@ -154,129 +280,4 @@ class TinyEkf {
 
         static bool did_finalize(float x[EKF_N], float A[EKF_N][EKF_N]);
 
-    public:
-
-        void initialize(
-                const uint32_t nowMsec,
-                const uint32_t predictionIntervalMsec,
-                const float min_covariance, 
-                const float max_covariance)
-        {
-            _predictionIntervalMsec = predictionIntervalMsec;
-
-            _isUpdated = false;
-
-            _min_covariance = min_covariance;
-            _max_covariance = max_covariance;
-
-            float diag[EKF_N] = {};
-
-            initialize_covariance_diagonal(diag);
-
-            for (uint8_t i=0; i<EKF_N; ++i) {
-
-                for (uint8_t j=0; j<EKF_N; ++j) {
-
-                    _p[i][j] = i==j ? diag[i] : 0;
-                }
-
-                _x[i] = 0;
-            }
-
-        }
-
-        void predict(const uint32_t nowMsec)
-        {
-            _isUpdated = true;
-
-            float xnew[EKF_N] = {};
-            float F[EKF_N][EKF_N] = {};
-            float Q[EKF_N][EKF_N] = {};
-            get_prediction(nowMsec, _x, xnew, F, Q);
-
-            // $\hat{x}_k = f(\hat{x}_{k-1})$
-            for (uint8_t i=0; i<EKF_N; ++i) {
-                _x[i] = xnew[i];
-            }
-
-            // # $P_k = F_{k-1} P_{k-1} F^T_{k-1} + Q_{k-1}$
-            multiplyCovariance(F);
-            add(_p, Q, _p);
-
-            cleanupCovariance();
-        }
-
-        void update(
-                const float hdat[EKF_N], 
-                const float error, 
-                const float stdMeasNoise)
-        {
-            float h[EKF_N] = {};
-            memcpy(h, hdat, EKF_N*sizeof(float));
-            float ph[EKF_N] = {};
-            multiply(_p, h, ph);
-            const auto r = stdMeasNoise * stdMeasNoise;
-            const auto hphr = r + dot(h, ph); // HPH' + R
-
-            float g[EKF_N] = {};
-            for (uint8_t i=0; i<EKF_N; ++i) {
-                g[i] = ph[i] / hphr;
-            }
-
-            // $\hat{x}_k = \hat{x_k} + G_k(z_k - h(\hat{x}_k))$
-            for (uint8_t i=0; i<EKF_N; ++i) {
-                _x[i] += g[i] * error;
-            }
-
-            float GH[EKF_N][EKF_N] = {};
-            outer(g, h, GH); 
-
-            for (int i=0; i<EKF_N; i++) { 
-                GH[i][i] -= 1;
-            }
-
-            // $P_k = (I - G_k H_k) P_k$
-            multiplyCovariance(GH);
-
-            // Add the measurement variance 
-            for (int i=0; i<EKF_N; i++) {
-                for (int j=0; j<EKF_N; j++) {
-                    _p[i][j] += j < i ? 0 : r * g[i] * g[j];
-                }
-            }
-
-            cleanupCovariance();
-
-            _isUpdated = true;
-        }
-
-        void finalize(void)
-        {
-            if (_isUpdated) {
-
-                float A[EKF_N][EKF_N] = {};
-
-                // Move attitude error into attitude if any of the angle errors are
-                // large enough
-                if (did_finalize(_x, A)) {
-
-                    multiplyCovariance(A);
-
-                    cleanupCovariance();
-                }
-
-                _isUpdated = false;
-            }
-        }
-
-        float * getState(void)
-        {
-            static float x[EKF_N];
-
-            for (uint8_t i=0; i<EKF_N; ++i) {
-                x[i] = _x[i];
-            }
-
-            return x;
-        }
 };
