23b22417b1cba6ec1c19184d66e697928114a4c4
simondlevy <simon.d.levy@gmail.com>
2024-05-12 20:54:46 -0400

    =Cleanup

--- src/TinyEKF.h
+++ src/TinyEKF.h
@@ -124,13 +124,13 @@ class TinyEKF {
         // http://jean-pierre.moreau.pagesperso-orange.fr/Cplus/choles_cpp.txt
 
         static int choldc1(float * a, float * p, int n) {
-            int i,j,k;
-            float sum;
 
-            for (i = 0; i < n; i++) {
-                for (j = i; j < n; j++) {
+            float sum = 0;
+
+            for (int i = 0; i < n; i++) {
+                for (int j = i; j < n; j++) {
                     sum = a[i*n+j];
-                    for (k = i - 1; k >= 0; k--) {
+                    for (int k = i - 1; k >= 0; k--) {
                         sum -= a[i*n+k] * a[j*n+k];
                     }
                     if (i == j) {
@@ -150,16 +150,17 @@ class TinyEKF {
 
         static int choldcsl(float * A, float * a, float * p, int n) 
         {
-            int i,j,k; float sum;
-            for (i = 0; i < n; i++) 
-                for (j = 0; j < n; j++) 
+            float sum = 0;
+
+            for (int i = 0; i < n; i++) 
+                for (int j = 0; j < n; j++) 
                     a[i*n+j] = A[i*n+j];
             if (choldc1(a, p, n)) return 1;
-            for (i = 0; i < n; i++) {
+            for (int i = 0; i < n; i++) {
                 a[i*n+i] = 1 / p[i];
-                for (j = i + 1; j < n; j++) {
+                for (int j = i + 1; j < n; j++) {
                     sum = 0;
-                    for (k = i; k < j; k++) {
+                    for (int k = i; k < j; k++) {
                         sum -= a[j*n+k] * a[k*n+i];
                     }
                     a[j*n+i] = sum / p[j];
@@ -172,26 +173,25 @@ class TinyEKF {
 
         static int cholsl(float * A, float * a, float * p, int n) 
         {
-            int i,j,k;
             if (choldcsl(A,a,p,n)) return 1;
-            for (i = 0; i < n; i++) {
-                for (j = i + 1; j < n; j++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
                     a[i*n+j] = 0.0;
                 }
             }
-            for (i = 0; i < n; i++) {
+            for (int i = 0; i < n; i++) {
                 a[i*n+i] *= a[i*n+i];
-                for (k = i + 1; k < n; k++) {
+                for (int k = i + 1; k < n; k++) {
                     a[i*n+i] += a[k*n+i] * a[k*n+i];
                 }
-                for (j = i + 1; j < n; j++) {
-                    for (k = j; k < n; k++) {
+                for (int j = i + 1; j < n; j++) {
+                    for (int k = j; k < n; k++) {
                         a[i*n+j] += a[k*n+i] * a[k*n+j];
                     }
                 }
             }
-            for (i = 0; i < n; i++) {
-                for (j = 0; j < i; j++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < i; j++) {
                     a[i*n+j] = a[j*n+i];
                 }
             }
@@ -204,33 +204,27 @@ class TinyEKF {
                 float * a, float * b, float * c, 
                 int arows, int acols, int bcols)
         {
-            int i, j,l;
-
-            for(i=0; i<arows; ++i)
-                for(j=0; j<bcols; ++j) {
+            for (int i=0; i<arows; ++i)
+                for (int j=0; j<bcols; ++j) {
                     c[i*bcols+j] = 0;
-                    for(l=0; l<acols; ++l)
+                    for (int l=0; l<acols; ++l)
                         c[i*bcols+j] += a[i*acols+l] * b[l*bcols+j];
                 }
         }
 
         static void mulvec(float * a, float * x, float * y, int m, int n)
         {
-            int i, j;
-
-            for(i=0; i<m; ++i) {
+            for (int i=0; i<m; ++i) {
                 y[i] = 0;
-                for(j=0; j<n; ++j)
+                for (int j=0; j<n; ++j)
                     y[i] += x[j] * a[i*n+j];
             }
         }
 
         static void transpose(float * a, float * at, int m, int n)
         {
-            int i,j;
-
-            for(i=0; i<m; ++i)
-                for(j=0; j<n; ++j) {
+            for (int i=0; i<m; ++i)
+                for (int j=0; j<n; ++j) {
                     at[j*m+i] = a[i*n+j];
                 }
         }
@@ -238,44 +232,35 @@ class TinyEKF {
         /* A <- A + B */
         static void accum(float * a, float * b, int m, int n)
         {        
-            int i,j;
-
-            for(i=0; i<m; ++i)
-                for(j=0; j<n; ++j)
+            for (int i=0; i<m; ++i)
+                for (int j=0; j<n; ++j)
                     a[i*n+j] += b[i*n+j];
         }
 
         /* C <- A + B */
         static void add(float * a, float * b, float * c, int n)
         {
-            int j;
-
-            for(j=0; j<n; ++j)
+            for (int j=0; j<n; ++j)
                 c[j] = a[j] + b[j];
         }
 
         /* C <- A - B */
         static void sub(float * a, float * b, float * c, int n)
         {
-            int j;
-
-            for(j=0; j<n; ++j)
+            for (int j=0; j<n; ++j)
                 c[j] = a[j] - b[j];
         }
 
         static void negate(float * a, int m, int n)
         {        
-            int i, j;
-
-            for(i=0; i<m; ++i)
-                for(j=0; j<n; ++j)
+            for (int i=0; i<m; ++i)
+                for (int j=0; j<n; ++j)
                     a[i*n+j] = -a[i*n+j];
         }
 
         static void mat_addeye(float * a, int n)
         {
-            int i;
-            for (i=0; i<n; ++i)
+            for (int i=0; i<n; ++i)
                 a[i*n+i] += 1;
         }
 
