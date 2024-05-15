538f21aea451bba7939357ef8c25404de5e4dfeb
simondlevy <simon.d.levy@gmail.com>
2024-05-15 13:09:00 -0400

    =Cleanup

--- c/tinyekf.h
+++ c/tinyekf.h
@@ -10,26 +10,23 @@
 
 typedef struct {
 
-    int n;           /* number of state values */
-    int m;           /* number of observables */
+    double x[EKF_N];     // state vector
 
-    double x[EKF_N];     /* state vector */
+    double P[EKF_N][EKF_N];  // prediction error covariance
+    double Q[EKF_N][EKF_N];  // process noise covariance
+    double R[EKF_M][EKF_M];  // measurement error covariance
 
-    double P[EKF_N][EKF_N];  /* prediction error covariance */
-    double Q[EKF_N][EKF_N];  /* process noise covariance */
-    double R[EKF_M][EKF_M];  /* measurement error covariance */
+    double G[EKF_N][EKF_M];  // Kalman gain; a.k.a. K
 
-    double G[EKF_N][EKF_M];  /* Kalman gain; a.k.a. K */
+    double F[EKF_N][EKF_N];  // Jacobian of process model
+    double H[EKF_M][EKF_N];  // Jacobian of measurement model
 
-    double F[EKF_N][EKF_N];  /* Jacobian of process model */
-    double H[EKF_M][EKF_N];  /* Jacobian of measurement model */
+    double Ht[EKF_N][EKF_M]; // transpose of measurement Jacobian
+    double Ft[EKF_N][EKF_N]; // transpose of process Jacobian
+    double Pp[EKF_N][EKF_N]; // P, post-prediction, pre-update
 
-    double Ht[EKF_N][EKF_M]; /* transpose of measurement Jacobian */
-    double Ft[EKF_N][EKF_N]; /* transpose of process Jacobian */
-    double Pp[EKF_N][EKF_N]; /* P, post-prediction, pre-update */
-
-    double fx[EKF_N];   /* output of user defined f() state-transition function */
-    double hx[EKF_M];   /* output of user defined h() measurement function */
+    double fx[EKF_N];   // output of user defined f() state-transition function
+    double hx[EKF_M];   // output of user defined h() measurement function
 
 } ekf_t;
 
