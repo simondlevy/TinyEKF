1422f0e72296e9f1da9dede1aaeb87e41064a028
simondlevy <simon.d.levy@gmail.com>
2024-05-15 13:48:06 -0400

    =Cleanup

--- c/tinyekf.c
+++ c/tinyekf.c
@@ -204,17 +204,17 @@ bool ekf_step(ekf_t * ekf, double * z)
     // P_k = F_{k-1} P_{k-1} F^T_{k-1} + Q_{k-1}
     mulmat((double *)ekf->F, (double *)ekf->P, tmp0, EKF_N, EKF_N, EKF_N);
     transpose(ekf->F, Ft, EKF_N, EKF_N);
-    mulmat((double *)tmp0, Ft, ekf->Pp, EKF_N, EKF_N, EKF_N);
-    accum((double *)ekf->Pp, ekf->Q, EKF_N, EKF_N);
+    mulmat((double *)tmp0, Ft, (double *)ekf->Pp, EKF_N, EKF_N, EKF_N);
+    accum((double *)ekf->Pp, (double *)ekf->Q, EKF_N, EKF_N);
 
     // G_k = P_k H^T_k (H_k P_k H^T_k + R)^{-1}
     transpose(ekf->H, Ht, EKF_M, EKF_N);
     mulmat((double *)ekf->Pp, Ht, tmp1, EKF_N, EKF_N, EKF_M);
-    mulmat((double *)ekf->H, ekf->Pp, tmp2, EKF_M, EKF_N, EKF_N);
+    mulmat((double *)ekf->H, (double *)ekf->Pp, tmp2, EKF_M, EKF_N, EKF_N);
     mulmat((double *)tmp2, Ht, tmp3, EKF_M, EKF_N, EKF_M);
-    accum((double *)tmp3, ekf->R, EKF_M, EKF_M);
+    accum((double *)tmp3, (double *)ekf->R, EKF_M, EKF_M);
     if (cholsl(tmp3, tmp4, tmp5, EKF_M)) return false;
-    mulmat((double *)tmp1, tmp4, ekf->G, EKF_N, EKF_M, EKF_M);
+    mulmat((double *)tmp1, tmp4, (double *)ekf->G, EKF_N, EKF_M, EKF_M);
 
     // \hat{x}_k = \hat{x_k} + G_k(z_k - h(\hat{x}_k))
     sub(z, ekf->hx, tmp5, EKF_M);
@@ -222,10 +222,10 @@ bool ekf_step(ekf_t * ekf, double * z)
     add(ekf->fx, tmp2, ekf->x, EKF_N);
 
     // P_k = (I - G_k H_k) P_k
-    mulmat((double *)ekf->G, ekf->H, tmp0, EKF_N, EKF_M, EKF_N);
+    mulmat((double *)ekf->G, (double *)ekf->H, tmp0, EKF_N, EKF_M, EKF_N);
     negate(tmp0, EKF_N, EKF_N);
     mat_addeye(tmp0, EKF_N);
-    mulmat((double *)tmp0, ekf->Pp, ekf->P, EKF_N, EKF_N, EKF_N);
+    mulmat((double *)tmp0, (double *)ekf->Pp, (double *)ekf->P, EKF_N, EKF_N, EKF_N);
 
     // success
     return true;
