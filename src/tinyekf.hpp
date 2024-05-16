a0a324193daca54d59f869b099799c749b1b086f
simondlevy <simon.d.levy@gmail.com>
2024-05-16 00:11:41 -0400

    Cleanup

--- src/tinyekf.hpp
+++ src/tinyekf.hpp
@@ -46,46 +46,6 @@ class TinyEKF {
             cleanupCovariance();
         }
 
-        bool update(
-                const float z[EKF_M], 
-                const float hx[EKF_M],
-                const float H[EKF_M*EKF_N],
-                const float R[EKF_M*EKF_M])
-        { 
-            float tmp0[EKF_N*EKF_N] = {};
-            float tmp1[EKF_N*EKF_M] = {};
-            float tmp2[EKF_M*EKF_N] = {};
-            float tmp3[EKF_M*EKF_M] = {};
-            float tmp4[EKF_M*EKF_M] = {};
-            float tmp5[EKF_M] = {}; 
-
-            float G[EKF_N*EKF_M] = {};  
-            float Ht[EKF_N*EKF_M] = {}; 
-
-            // G_k = P_k H^T_k (H_k P_k H^T_k + R)^{-1}
-            transpose(H, Ht, EKF_M, EKF_N);
-            mulmat(_P, Ht, tmp1, EKF_N,EKF_N, EKF_M);
-            mulmat(H, _P, tmp2, EKF_M,EKF_N, EKF_N);
-            mulmat(tmp2, Ht, tmp3, EKF_M,EKF_N, EKF_M);
-            accum(tmp3, R, EKF_M, EKF_M);
-            if (cholsl(tmp3, tmp4, tmp5, EKF_M)) return false;
-            mulmat(tmp1, tmp4, G, EKF_N,EKF_M, EKF_M);
-
-            // \hat{x}_k = \hat{x_k} + G_k(z_k - h(\hat{x}_k))
-            sub(z, hx, tmp5, EKF_M);
-            mulvec(G, tmp5, tmp2, EKF_N, EKF_M);
-            add(_x, tmp2, _x, EKF_N);
-
-            // P_k = (I - G_k H_k) P_k
-            mulmat(G, H, tmp0, EKF_N,EKF_M, EKF_N);
-            negate(tmp0, EKF_N, EKF_N);
-            mat_addeye(tmp0, EKF_N);
-            mulmat(tmp0, _P, _P, EKF_N,EKF_N, EKF_N);
-
-            // success
-            return true;
-        }
-
          void update_with_scalar(
                 const float h[EKF_N], 
                 const float z,
@@ -204,85 +164,6 @@ class TinyEKF {
                 }
             }
         } 
-
-        // Cholesky-decomposition matrix-inversion code, adapated from
-        // http://jean-pierre.moreau.pagesperso-orange.fr/Cplus/choles_cpp.txt
-
-        static int choldc1(float * a, float * p, const int n) {
-
-            float sum = 0;
-
-            for (int i = 0; i < n; i++) {
-                for (int j = i; j < n; j++) {
-                    sum = a[i*n+j];
-                    for (int k = i - 1; k >= 0; k--) {
-                        sum -= a[i*n+k] * a[j*n+k];
-                    }
-                    if (i == j) {
-                        if (sum <= 0) {
-                            return 1; /* error */
-                        }
-                        p[i] = sqrt(sum);
-                    }
-                    else {
-                        a[j*n+i] = sum / p[i];
-                    }
-                }
-            }
-
-            return 0; /* success */
-        }
-
-        static int choldcsl(const float * A, float * a, float * p, const int n) 
-        {
-            float sum = 0;
-
-            for (int i = 0; i < n; i++) 
-                for (int j = 0; j < n; j++) 
-                    a[i*n+j] = A[i*n+j];
-            if (choldc1(a, p, n)) return 1;
-            for (int i = 0; i < n; i++) {
-                a[i*n+i] = 1 / p[i];
-                for (int j = i + 1; j < n; j++) {
-                    sum = 0;
-                    for (int k = i; k < j; k++) {
-                        sum -= a[j*n+k] * a[k*n+i];
-                    }
-                    a[j*n+i] = sum / p[j];
-                }
-            }
-
-            return 0; /* success */
-        }
-
-
-        static int cholsl(const float * A, float * a, float * p, const int n) 
-        {
-            if (choldcsl(A,a,p,n)) return 1;
-            for (int i = 0; i < n; i++) {
-                for (int j = i + 1; j < n; j++) {
-                    a[i*n+j] = 0.0;
-                }
-            }
-            for (int i = 0; i < n; i++) {
-                a[i*n+i] *= a[i*n+i];
-                for (int k = i + 1; k < n; k++) {
-                    a[i*n+i] += a[k*n+i] * a[k*n+i];
-                }
-                for (int j = i + 1; j < n; j++) {
-                    for (int k = j; k < n; k++) {
-                        a[i*n+j] += a[k*n+i] * a[k*n+j];
-                    }
-                }
-            }
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < i; j++) {
-                    a[i*n+j] = a[j*n+i];
-                }
-            }
-
-            return 0; /* success */
-        }
 
         // C <- A * B
         static void mulmat(
